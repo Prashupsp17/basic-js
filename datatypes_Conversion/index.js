50c7bfe93c33588365de42b41a727a8b53e3a5ed
Prashupsp17 <shindeprashant967@gmail.com>
2023-08-26 19:59:33 +0530

    datatypes_conversion

---
+++ datatypes_Conversion/index.js
@@ -0,0 +1,33 @@
+let score = 33
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let scores = "33"
+// console.log(typeof scores);
+
+let valueInNumber = Number(scores);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let value = undefined;
+console.log(value);
+
+let firstname ="Prashant"
+console.log(firstname);
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let login = "Prashant"
+let logins = Boolean(login);
+console.log(logins);
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
